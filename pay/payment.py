9ca57050c488c1acea41ef274102c0f2269a546c
luis2003 <luis2003@gmail.com>
2023-01-18 07:36:21 -0300

    Add dependency injection to pay_order and create a processor interface

--- pay/payment.py
+++ pay/payment.py
@@ -1,13 +1,19 @@
+from typing import Protocol
+
 from pay.order import Order
 from pay.processor import PaymentProcessor
 
 
-def pay_order(order: Order):
+class PaymentProcessor(Protocol):
+    def charge(self, card: str, month: int, year: int, amount: int) -> None:
+        """Charges the card with the amount."""
+
+
+def pay_order(order: Order, processor: PaymentProcessor):
     if order.total == 0:
         raise ValueError("Can't pay an order with total of 0.")
     card = input("Please enter your card number: ")
     month = int(input("Please enter the card expiry month: "))
     year = int(input("Please enter the card expiry year: "))
-    payment_processor = PaymentProcessor("6cfb67f3-6281-4031-b893-ea85db0dce20")
-    payment_processor.charge(card, month, year, amount=order.total)
+    processor.charge(card, month, year, amount=order.total)
     order.pay()
