5f1e3a70cc98e10b40a9a69baaf977b9dcefe93f
luis2003 <luis2003@gmail.com>
2023-01-20 07:49:20 -0300

    Add test fixtures for CreditCard

--- pay/tests/test_payment.py
+++ pay/tests/test_payment.py
@@ -1,6 +1,7 @@
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
+from pay.credit_card import CreditCard
 from pay.order import Order, LineItem
 from pay.payment import pay_order
 
@@ -10,20 +11,21 @@ class PaymentProcessorMock:
         print(f"Charging {card} with amount ${amount/100:.2f}.")
 
 
-def test_pay_order(monkeypatch: MonkeyPatch) -> None:
-    inputs = ["1249190007575069", "12", "2024"]
-    monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
+@pytest.fixture
+def card() -> CreditCard:
+    return CreditCard("1249190007575069", 12, 2024)
+
+
+def test_pay_order(card: CreditCard) -> None:
     order = Order()
     order.line_items.append(LineItem("Test", 100))
-    pay_order(order, PaymentProcessorMock())
+    pay_order(order, card, PaymentProcessorMock())
 
 
-def test_pay_order_invalid_order_no_line_items(monkeypatch: MonkeyPatch) -> None:
+def test_pay_order_invalid_order_no_line_items(card: CreditCard) -> None:
     with pytest.raises(ValueError):
-        inputs = ["1249190007575069", "12", "2024"]
-        monkeypatch.setattr("builtins.input", lambda _: inputs.pop(0))
         order = Order()
-        pay_order(order, PaymentProcessorMock())
+        pay_order(order, card, PaymentProcessorMock())
 
 
 
--- pay/tests/test_processor.py
+++ pay/tests/test_processor.py
@@ -1,24 +1,36 @@
 import pytest
+
+from pay.credit_card import CreditCard
 from pay.processor import PaymentProcessor
 
 API_KEY = "6cfb67f3-6281-4031-b893-ea85db0dce20"  # should not be commited to prod code
 
 
-def test_invalid_api_key() -> None:
+@pytest.fixture
+def card() -> CreditCard:
+    return CreditCard("1249190007575069", 12, 2024)
+
+
+@pytest.fixture
+def invalid_number_and_date_card() -> CreditCard:
+    return CreditCard("1249190007575068", 12, 1900)
+
+
+def test_invalid_api_key(card: CreditCard) -> None:
     with pytest.raises(ValueError):
         processor = PaymentProcessor("")
-        processor.charge("1249190007575069", 12, 2024, 100)
+        processor.charge(card, 100)
 
 
-def test_charge_valid_date_card() -> None:
+def test_charge_valid_date_card(card: CreditCard) -> None:
     processor = PaymentProcessor(API_KEY)
-    processor.charge("1249190007575069", 12, 2024, 100)
+    processor.charge(card, 100)
 
 
-def test_charge_invalid_date_card() -> None:
+def test_charge_invalid_date_card(invalid_number_and_date_card: CreditCard) -> None:
     with pytest.raises(ValueError):
         processor = PaymentProcessor(API_KEY)
-        processor.charge("1249190007575069", 12, 1900, 100)
+        processor.charge(invalid_number_and_date_card, 100)
 
 
 def test_luhn_checksum_invalid_card() -> None:
@@ -31,7 +43,7 @@ def test_luhn_checksum_valid_card() -> None:
     assert processor.luhn_checksum("1249190007575069")
 
 
-def test_charge_invalid_card_number() -> None:
+def test_charge_invalid_card_number(invalid_number_and_date_card) -> None:
     with pytest.raises(ValueError):
         processor = PaymentProcessor(API_KEY)
-        processor.charge("1249190007575068", 12, 1900, 100)
+        processor.charge(invalid_number_and_date_card, 100)
